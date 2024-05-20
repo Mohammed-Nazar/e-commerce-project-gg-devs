eb0c4262098574bd5a2f7ffb6131ec5201425069
Zainab Alnajjar <eng.zainab.alnajar@gmail.com>
2024-05-20 17:20:15 +0300

    fix duplecated files

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,17 +1,28 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const customerSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },   // (unique: true) cause an error 
+    email: { type: String, required: true },
     password: { type: String, required: true },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    img: { type: String },
+    gender: {type:String, enum: ["Male", "Female", "Rather not to say"], default: "Rather not to say"},
+    // img: { type: String },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("customer", customerSchema);
+// Pre-save to hash the password
+customerSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Method to compare passwords 4 validtion 
+customerSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('customer', customerSchema);
