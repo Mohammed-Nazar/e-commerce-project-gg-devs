25a193784b9c6528c7be84716a4cb96dea3f9a4c
maram qais <maramqaiszebar@gmail.com>
2024-05-21 23:33:29 +0300

    customers stuff

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,21 +1,36 @@
 const mongoose = require("mongoose");
 
-const { MONGODB_USER, MONGODB_PASSWORD, DB_HOST, DB_PORT, MONGODB_DATABASE, TEST_DB_HOST } = process.env;
+// const { MONGODB_USER, MONGODB_PASSWORD, DB_HOST, DB_PORT, MONGODB_DATABASE, TEST_DB_HOST } = process.env;
 
-const DB_URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST}:${DB_PORT}/${MONGODB_DATABASE}?authSource=admin`;
+// const DB_URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST}:${DB_PORT}/${MONGODB_DATABASE}?authSource=admin`;
+
+// const connectToMongo = () => {
+//   mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//   const db = mongoose.connection;
+
+//   db.once("open", () => {
+//     console.log("Database connected: ", DB_URI);
+//   });
+
+//   db.on("error", (err) => {
+//     console.error("Database connection error: ", err);
+//   });
+// };
+
+// module.exports = connectToMongo;
+
+const DB_URI = "mongodb+srv://ggdevs:1234@e-commerce.esetu5o.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce&ssl=true";
+
+const url = DB_URI;
 
 const connectToMongo = () => {
-  mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  const db = mongoose.connection;
-
-  db.once("open", () => {
-    console.log("Database connected: ", DB_URI);
-  });
-
-  db.on("error", (err) => {
-    console.error("Database connection error: ", err);
-  });
+  mongoose.connect(url)
+  .then(()=>{
+    console.log("Conneted to DB");
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 };
-
 module.exports = connectToMongo;
